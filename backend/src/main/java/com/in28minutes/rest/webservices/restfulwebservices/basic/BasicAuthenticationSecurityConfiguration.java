665e838be464d5a06852f8226cf951bc1355dc65
아크케 <seetimelord@naver.com>
2023-11-01 18:04:00 +0900

    퇴근전에
    commit

---
+++ backend/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.in28minutes.rest.webservices.restfulwebservices.basic;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthenticationSecurityConfiguration {
+
+	//Filter chain
+	// authenticate all request
+	// basic authentication
+	// disalbling csrf
+	//stabkess rest api
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http)throws Exception {
+		return
+			http.authorizeHttpRequests(
+					auth -> auth.anyRequest().authenticated()
+				)
+				.httpBasic(Customizer.withDefaults())
+
+				.sessionManagement(
+					session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.csrf().disable()
+					.build();
+	}
+}
