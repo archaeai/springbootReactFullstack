5665a156d255abdd97b12f8d889a2e81ef37ee1e
아크케 <seetimelord@naver.com>
2023-11-07 11:12:20 +0900

    UserDetail을 통해서 user 생성
    UserDetail과 DataSource를 통해서 user을 생성하고, 그 생성된 유저가 자동으로 User, Authorities 테이블에 insert됨.

--- backend/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
+++ backend/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -27,7 +28,7 @@ public class BasicAuthenticationSecurityConfiguration {
 
 					.sessionManagement(
 						session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-					.csrf((csrf) -> csrf.disable())
+					.csrf(AbstractHttpConfigurer::disable)
 					.build();
 	}
 }
--- learn-spring-security/src/main/java/com/minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -5,13 +5,21 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -21,21 +29,25 @@ public class BasicAuthSecurityConfiguration {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((auth) ->
-                auth.anyRequest().authenticated());
+                auth
+                        //.requestMatchers(new AntPathRequestMatcher("/h2-console/**"))
+                        .anyRequest().authenticated()
+        );
+
         //http.formLogin(withDefaults());
         http.sessionManagement(
                 session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
         http.httpBasic(withDefaults());
-        http.csrf(
-                AbstractHttpConfigurer::disable
-        );
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers(headers ->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+
 
         return http.build();
     }
     @Bean
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService(DataSource dataSource) {
         var user = User.withUsername("in28minutes")
                 .password("{noop}dummy")
                 .roles("USER")
@@ -45,6 +57,17 @@ public class BasicAuthSecurityConfiguration {
                 .password("{noop}dummy")
                 .roles("ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(user,admin);
+        var jdbcUserDetailManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailManager.createUser(user);
+        jdbcUserDetailManager.createUser(admin);
+        return jdbcUserDetailManager;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
     }
 }
